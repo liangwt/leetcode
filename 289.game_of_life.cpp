ccc5355785da689827244c7e2800c922d898e029
liangwt <liang.w.t@foxmail.com>
2020-04-02 15:08:04 +0800

    feat: 289. Game of Life

--- 289.game_of_life.cpp
+++ 289.game_of_life.cpp
@@ -1,48 +1,98 @@
-/**
- * According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
- *
- * Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
- *
- * Any live cell with fewer than two live neighbors dies, as if caused by under-population.
- * Any live cell with two or three live neighbors lives on to the next generation.
- * Any live cell with more than three live neighbors dies, as if by over-population..
- * Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
- * Write a function to compute the next state (after one update) of the board given its current state. The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
- *
- * Example:
- *
- * Input:
- * [
- *   [0,1,0],
- *   [0,0,1],
- *   [1,1,1],
- *   [0,0,0]
- * ]
- * Output:
- * [
- *   [0,0,0],
- *   [1,0,1],
- *   [0,1,1],
- *   [0,1,0]
- * ]
- * Follow up:
- *
- * Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
- * In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
- */
-
 #include <vector>
+#include <iostream>
+#include <assert.h>
 
 using namespace std;
 
-class Solution {
-public:
-    void gameOfLife(vector<vector<int>>& board) {
+class Solution
+{
+private:
+    int x_len;
+    int y_len;
 
+public:
+    void gameOfLife(vector<vector<int>> &board)
+    {
+        x_len = board[0].size();
+        y_len = board.size();
+        auto copy_board = board;
+
+        for (int i = 0; i < y_len; ++i)
+        {
+            for (int j = 0; j < x_len; ++j)
+            {
+                int nums = calLive(copy_board, i, j);
+
+                if (copy_board[i][j] == 1)
+                {
+                    if (nums < 2)
+                    {
+                        board[i][j] = 0;
+                    }
+                    else if (nums >= 2 && nums <= 3)
+                    {
+                        board[i][j] = 1;
+                    }
+                    else
+                    {
+                        board[i][j] = 0;
+                    }
+                }
+                else
+                {
+                    if (nums == 3)
+                    {
+                        board[i][j] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    // cal the live cell beyond eight locations
+    int calLive(vector<vector<int>> &board, int i, int j)
+    {
+        vector<pair<int, int>> dir = {
+            {-1, -1},
+            {-1, 0},
+            {-1, 1},
+            {0, -1},
+            {0, 1},
+            {1, -1},
+            {1, 0},
+            {1, 1},
+        };
+
+        int nums = 0;
+        for (auto [dx, dy] : dir)
+        {
+            int xx = j + dx;
+            int yy = i + dy;
+            if (xx >= 0 && xx < x_len && yy >= 0 && yy < y_len && board[yy][xx] == 1)
+            {
+                nums++;
+            }
+        }
+
+        return nums;
     }
 };
 
-int main(){
+int main()
+{
+    Solution s;
 
-    return 0;
+    vector<vector<int>> b1 = {
+        {0, 1, 0},
+        {0, 0, 1},
+        {1, 1, 1},
+        {0, 0, 0}};
+    vector<vector<int>> ans1 = {
+        {0, 0, 0},
+        {1, 0, 1},
+        {0, 1, 1},
+        {0, 1, 0}};
+
+    s.gameOfLife(b1);
+    assert(b1 == ans1);
 }
